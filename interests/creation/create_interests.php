c57997d9daf7d45aae65e2e4deea5d801a267e78
Fabien Sabatié <fabien.sabatie@live.fr>
2017-08-24 00:28:58 +0200

    Initial commit

--- interests/creation/create_interests.php
+++
@@ -1,7 +0,0 @@
-<?php
-  set_include_path('/Users/aidenpearce/QuasR/');
-  require_once 'SRC/includes.php';
-  ft_check_if_logged();
-
-  
-?>
--- interests/creation/ft_create_interests.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-  require_once 'src/includes.php';
-
-
-
-
- ?>
