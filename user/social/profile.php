21cc16afb8506a9fded7c455a6c8e63c6927236f
Fabien Sabatié <fabien.sabatie@live.fr>
2017-08-28 00:13:50 +0200

    Added instant messages right panel

--- user/social/profile.php
+++ user/social/profile.php
@@ -10,15 +10,19 @@
     <title>QuasR | Social</title>
     <link rel="stylesheet" type="text/css" href="/assets/css/main_social.css">
     <link rel="stylesheet" type="text/css" href="/assets/css/side_menu_main.css">
+    <link rel="stylesheet" type="text/css" href="/assets/css/messages_sidebar.css">
   </head>
   <body>
     <div id="main_container">
       <?php require_once 'src/sidebar_menu.php'; ?>
       <div id="social_container">
         <?php
-          echo 'Hello ' . $_SESSION["user_first_name"] . ', your username is ' . $_SESSION["username"] . '.<br>';
+          echo '<span class="social_content">Hello '
+          . $_SESSION["user_first_name"] . ', welcome back to QuasR !<br><br>'
+          . '<img src="/assets/images/rocks_and_rocket.png"></span>';
         ?>
       </div>
+      <div id="messages_sidebar"></div>
     </div>
   </body>
 </html>
